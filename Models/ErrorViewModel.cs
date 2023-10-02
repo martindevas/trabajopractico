4db2addf042c36ec8953ac97126c171243951211
unknown <martindevas@istea.com>
2023-10-02 20:56:01 -0300

    primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace trabajopractico.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
